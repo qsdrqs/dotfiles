56bffe4037eed84658968676bd5df971262a39e1
qsdrqs <qsdrqs@gmail.com>
2024-01-08 04:41:59 -0600

    update yazi

---
+++ yazi/plugins/mime.yazi/init.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+function M:preload()
+	local urls = {}
+	for _, file in ipairs(self.files) do
+		urls[#urls + 1] = tostring(file.url)
+	end
+
+	local args
+	if ya.target_family() == "windows" then
+		args = { "-b", "--mime-type" }
+	else
+		args = { "-b", "--mime-type" }
+	end
+
+	local child, code = Command("file"):args(args):args(urls):stdout(Command.PIPED):spawn()
+	if not child then
+		ya.err("spawn `file` command returns " .. tostring(code))
+		return 0
+	end
+
+	local mimes, last = {}, ya.time()
+	local flush = function(force)
+		if not force and ya.time() - last < 0.1 then
+			return
+		end
+		if next(mimes) then
+			ya.manager_emit("update_mimetype", {}, mimes)
+			mimes, last = {}, ya.time()
+		end
+	end
+
+	local i, j = 1, 0
+	repeat
+		local next, event = child:read_line_with { timeout = 100 }
+		if event == 3 then
+			flush(true)
+			goto continue
+		elseif event ~= 0 then
+			break
+		end
+
+		next = next:gsub("[\r\n]+$", "")
+		if ya.mime_valid(next) then
+			j, mimes[urls[i]] = j + 1, next
+			flush(false)
+		end
+
+		i = i + 1
+		::continue::
+	until i > #urls
+
+	flush(true)
+	return j == #urls and 3 or 2
+end
+
+return M
