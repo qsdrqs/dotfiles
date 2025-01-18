a5f1e3277dfc7bd3b7fc55c0122e970f11ea63b0
qsdrqs <qsdrqs@gmail.com>
2025-01-18 17:17:18 -0600

    update nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
-    nixpkgs-last.url = "github:NixOS/nixpkgs/6df24922a1400241dae323af55f30e4318a6ca65";
+    nixpkgs-last.url = "github:NixOS/nixpkgs/bffc22eb12172e6db3c5dde9e3e5628f8e3e7912";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     # Specific commits to fix the version of some packages.
     nixpkgs-firefox-dev.url = "github:NixOS/nixpkgs/18536bf04cd71abd345f9579158841376fdd0c5a";
