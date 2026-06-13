31aff66763f4c252ccba12a2b9c36787d3b8874f
qsdrqs <qsdrqs@gmail.com>
2026-06-13 15:49:19 -0500

    update

--- nixos/laptop-configuration.nix
+++ nixos/laptop-configuration.nix
@@ -14,17 +14,6 @@ let
   niriMonitorPower = pkgs.writeShellScriptBin "niri-monitor-power" ''
     exec ${pkgs.python3}/bin/python3 ${./scripts/niri-monitor-power.py}
   '';
-  shutdownHardTimeout = pkgs.writeShellScript "shutdown-hard-timeout" ''
-    set -eu
-
-    trigger="$1"
-
-    (
-        ${pkgs.coreutils}/bin/sleep 600
-        ${pkgs.coreutils}/bin/sync
-        printf '%s' "$trigger" > /proc/sysrq-trigger
-    ) &
-  '';
 in
 {
   environment.systemPackages = with pkgs; [
@@ -80,46 +69,21 @@ in
   };
 
   systemd = {
-    services = {
-      poweroff-hard-timeout = {
-        description = "Force poweroff if shutdown is still running after 10 minutes";
-        wantedBy = [
-          "poweroff.target"
-          "halt.target"
-        ];
-        before = [
-          "poweroff.target"
-          "halt.target"
-        ];
-        conflicts = [
-          "reboot.target"
-          "kexec.target"
-        ];
-        unitConfig.DefaultDependencies = false;
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          KillMode = "none";
-          ExecStart = "${shutdownHardTimeout} o";
-        };
+    # Set timeouts to 10 minutes and force the action if it fails, to avoid hanging on shutdown/reboot for too long
+    targets = {
+      reboot.unitConfig = {
+        JobTimeoutSec = "10min";
+        JobTimeoutAction = "reboot-force";
       };
 
-      reboot-hard-timeout = {
-        description = "Force reboot if reboot is still running after 10 minutes";
-        wantedBy = [ "reboot.target" ];
-        before = [ "reboot.target" ];
-        conflicts = [
-          "poweroff.target"
-          "halt.target"
-          "kexec.target"
-        ];
-        unitConfig.DefaultDependencies = false;
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          KillMode = "none";
-          ExecStart = "${shutdownHardTimeout} b";
-        };
+      poweroff.unitConfig = {
+        JobTimeoutSec = "10min";
+        JobTimeoutAction = "poweroff-force";
+      };
+
+      halt.unitConfig = {
+        JobTimeoutSec = "10min";
+        JobTimeoutAction = "halt-force";
       };
     };
 
